ad65de05d720a6b214c116f97d2b275bfc12609f
vdtyson <versilistyson@gmail.com>
2020-04-07 00:43:09 -0400

    - Changes Authentication State to single Data Class

--- AndroidStreetEats/app/src/main/java/com/versilistyson/androidstreeteats/SetupAccountDialogFragment.kt
+++ AndroidStreetEats/app/src/main/java/com/versilistyson/androidstreeteats/SetupAccountDialogFragment.kt
@@ -1,26 +1,53 @@
 package com.versilistyson.androidstreeteats
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.DialogFragment
 import com.versilistyson.androidstreeteats.databinding.FragmentSetupAccountDialogBinding
+import com.versilistyson.androidstreeteats.presentation.util.addOnBackPressedCallBack
 
 
 class SetupAccountDialogFragment : DialogFragment() {
 
-    lateinit var setupAccountDialogBinding: FragmentSetupAccountDialogBinding
+    private lateinit var binding: FragmentSetupAccountDialogBinding
+    private val finishActivityOnBackPressedCallback: OnBackPressedCallback =
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        setupAccountDialogBinding = FragmentSetupAccountDialogBinding.inflate(inflater, container, false)
-        setupAccountDialogBinding.lifecycleOwner = this
-        return setupAccountDialogBinding.root
+        binding = FragmentSetupAccountDialogBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupButtons()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        addOnBackPressedCallBack(finishActivityOnBackPressedCallback)
+    }
+
+    private fun setupButtons() {
+        binding.bttnExit.setOnClickListener {
+            requireActivity().finish()
+        }
+        binding.bttnSetupAccount.setOnClickListener {
+            TODO("Go to setup page")
+        }
+    }
+
 
 }
